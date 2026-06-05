0a915cf1ce7686c684c4c6d9cb738a39d34db38a
shuvro18 <shuvrobiswas404@gmail.com>
2026-06-05 22:24:47 +0600

    show details page edited

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -9,11 +9,11 @@ import { HiStar } from "react-icons/hi";
 const Products = async () => {
     const res = await fetch("http://localhost:3000/data.json");
     const product = await res.json();
-    // console.log(product);
+    // console.log(product);className="inline-flex items-center gap-2 text-xs font-bold tracking-wider text-gray-400 hover:text-[#5C1D24] transition-colors uppercase mb-8"
     return (
         <div>
             
-            <p className=" text-4xl text-center font-bold">All Products</p>
+            <p className=" text-4xl text-center font-bold tracking-wider text-gray-400 hover:text-[#5C1D24] transition-colors uppercase ">All Products</p>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-10 container mx-auto py-10">
                 {product.map((product) => (
                     <div key={product.id} className="group  flex flex-col justify-between border border-secondary rounded-2xl">
