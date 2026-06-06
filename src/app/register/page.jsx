037d2d539d0571a6b0287c0c8985f0723857b31b
shuvro18 <shuvrobiswas404@gmail.com>
2026-06-06 10:38:52 +0600

    google based register added

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -49,6 +49,15 @@ const RegisterPage = () => {
         }
     };
 
+
+
+    //Google based register handler
+     const HandleGoogleLogin = async () => {
+        const data = await authClient.signIn.social({
+          provider: "google",
+        });
+      }
+
     return (
         <div className="min-h-screen bg-linear-to-br from-slate-900 via-purple-950 to-slate-900 flex items-center justify-center px-4 py-12 relative overflow-hidden">
 
@@ -179,15 +188,12 @@ const RegisterPage = () => {
                 <div className="divider my-6 text-slate-500 text-xs font-light">OR SIGN UP WITH</div>
 
                 {/* সোশ্যাল রেজিস্টার বাটন (Better-Auth এর signIn.social() এর জন্য পারফেক্ট) */}
-                <div className="grid grid-cols-2 gap-3">
-                    <button className="btn btn-outline border-white/10 hover:border-white/20 bg-slate-950/20 text-white rounded-xl gap-2 font-medium text-xs h-11 min-h-0 normal-case">
+                <div className="">
+                    <button onClick={HandleGoogleLogin} className="w-full btn btn-outline border-white/10 hover:border-white/20 bg-slate-950/20 text-white rounded-xl gap-2 font-medium text-xs h-11 min-h-0 normal-case">
                         <FcGoogle className="text-lg" />
                         Google
                     </button>
-                    <button className="btn btn-outline border-white/10 hover:border-white/20 bg-slate-950/20 text-white rounded-xl gap-2 font-medium text-xs h-11 min-h-0 normal-case">
-                        <FaGithub className="text-lg" />
-                        GitHub
-                    </button>
+                    
                 </div>
 
                 {/* লগইন পেজের লিংক */}
